691c5d1fc3a1bd09b617f2421aeeaac239368718
Wesley Carvalho <wesleycarvalho.dev@outlook.com>
2022-06-13 10:53:17 -0300

    Efeito maquina de escrever

--- script.js
+++ script.js
@@ -1,51 +1,65 @@
-const elementoResposta = document.querySelector("#resposta")
-const inputPergunta = document.querySelector("#inputPergunta")
+const elementoResposta = document.querySelector('#resposta')
+const inputPergunta = document.querySelector('#inputPergunta')
 const buttonPerguntar = document.querySelector('#buttonPerguntar')
 const respostas = [
-  "Certeza!",
-  "Não tenho tanta certeza.",
-  "É decididamente assim.",
-  "Não conte com isso.",
-  "Sem dúvidas!",
-  "Pergunte novamente mais tarde.",
-  "Sim, definitivamente!",
-  "Minha resposta é não.",
-  "Você pode contar com isso.",
-  "Melhor não te dizer agora.",
-  "A meu ver, sim.",
-  "Minhas fontes dizem não.",
-  "Provavelmente.",
-  "Não é possível prever agora.",
-  "Perspectiva boa.",
-  "As perspectivas não são tão boas.",
-  "Sim.",
-  "Concentre-se e pergunte novamente.",
-  "Sinais apontam que sim.",
+  'Certeza!',
+  'Não tenho tanta certeza.',
+  'É decididamente assim.',
+  'Não conte com isso.',
+  'Sem dúvidas!',
+  'Pergunte novamente mais tarde.',
+  'Sim, definitivamente!',
+  'Minha resposta é não.',
+  'Você pode contar com isso.',
+  'Melhor não te dizer agora.',
+  'A meu ver, sim.',
+  'Minhas fontes dizem não.',
+  'Provavelmente.',
+  'Não é possível prever agora.',
+  'Perspectiva boa.',
+  'As perspectivas não são tão boas.',
+  'Sim.',
+  'Concentre-se e pergunte novamente.',
+  'Sinais apontam que sim.'
 ]
 
 // clicar em fazer pergunta
 function fazerPergunta() {
-
-  if(inputPergunta.value == "") {
-    alert("Digite sua pergunta")
+  if (inputPergunta.value == '') {
+    alert('Digite sua pergunta')
     return
   }
 
-  buttonPerguntar.setAttribute("disabled", true)
+  buttonPerguntar.setAttribute('disabled', true)
 
-  const pergunta = "<div>" + inputPergunta.value + "</div>"
+  const pergunta = '<div>' + inputPergunta.value + '</div>'
 
   // gerar numero aletorio
-  const  totalRespostas = respostas.length
+  const totalRespostas = respostas.length
   const numeroAleatorio = Math.floor(Math.random() * totalRespostas)
 
   elementoResposta.innerHTML = pergunta + respostas[numeroAleatorio]
 
-  elementoResposta.style.opacity = 1;
+  elementoResposta.style.opacity = 1
 
   // sumir a resposta depois de 3 segundos
-  setTimeout(function() {
-    elementoResposta.style.opacity = 0;
-    buttonPerguntar.removeAttribute("disabled")
+  setTimeout(function () {
+    elementoResposta.style.opacity = 0
+    buttonPerguntar.removeAttribute('disabled')
   }, 3000)
 }
+
+// efeito maquina de escrever
+
+function typeWrite(elemento) {
+  const textoArray = elemento.innerHTML.split('')
+  elemento.innerHTML = ' '
+  textoArray.forEach(function (letra, i) {
+    setTimeout(function () {
+      elemento.innerHTML += letra
+    }, 80 * i)
+  })
+}
+
+const titulo = document.querySelector('.titulo-principal')
+typeWrite(titulo)
